d5f0775a5ca533a8a9e670c7f97da863b574f09b
NataliaFomina <38000375+NataliaFomina@users.noreply.github.com>
2018-12-21 15:48:38 +0300

    Create money.php

---
+++ 1.3/money/money.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<?php
+
+if ( (count($argv) <= 2 && $argv[1] !== '--today') ) {
+  echo 'Ошибка! Аргументы не заданы. Укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки}';
+} else if ($argv[1] === '--today') {
+  costsOfDay($argv);
+} else {
+  addCosts($argv);
+}
+
+// функция расходов за день
+function costsOfDay($argv) 
+{
+  $resource = fopen('money/money.csv', 'r');
+  $data = [];
+  // скачиваем данные
+  while (!feof($resource)) {
+    $fileStr = fgetcsv($resource);
+    if ($fileStr != false) {
+      $data[] = $fileStr;
+    }
+  }
+  fclose($resource);
+
+  $count = 0;
+  $today = str_replace("-", ".", date('Y-m-d'));
+  foreach ($data as $item) {
+    if ($today === $item[0]) {
+      $count += (int) $item[1];
+    }
+  }
+  if ($count === 0) {
+    echo "$today расходов не было!";
+  } else {
+    echo "$today расход за день: $count";
+  }
+}
+
+// функция добавления расходов
+function addCosts($argv) 
+{
+  $today = str_replace("-", ".", date('Y-m-d'));
+  $name = implode(" ", array_slice($argv, 2));  
+  $data = [ $today, $argv[1], $name ];
+
+  $resource = fopen('money/money.csv', 'a+');
+  fputcsv($resource, $data);
+  fclose($resource);
+
+  echo "Добавлена строка: $data[0], $data[1], $data[2]";
+}
+?>
+    
+</body>
+</html>
