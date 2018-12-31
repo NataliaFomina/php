5ce67479505c81e895b952404e31078bb55958e8
NataliaFomina <38000375+NataliaFomina@users.noreply.github.com>
2018-12-31 18:02:05 +0300

    Update test.php

--- 2.2/test.php
+++ 2.2/test.php
@@ -1,14 +1,58 @@
 <?php
-$json = file_get_contents( "tmp/test1.json");
-$jsonData = json_decode($json, true);
-// var_dump($jsonData);
 
-if (!empty($POST)) {
-  echo $POST;
-  var_dump($POST);
-  $e = (int)$POST['1'];
-  var_dump($e);
+function getJson($name) 
+{
+  $files = glob('tmp/*.json');
+  $jsonName;
+
+  foreach($files as $file) {
+    $fileName = explode('.', explode('/', $file)[1])[0];
+    if ($fileName === $name) {
+      $jsonName = "$name.json" ;
+    }
+  }
+  $jsonData = file_get_contents("tmp/$jsonName");
+  $json = json_decode($jsonData, true);
+  $json['fileName'] = $name;
+  // print_r($json);
+  return $json;
 }
+
+if (!empty($_GET)) {
+  $name = $_GET['fileName'];
+  $json = getJson($name);
+}
+
+if (!empty($_POST)) {
+  // print_r($_POST);
+  $name = $_POST['fileName'];
+  $json = getJson($name);
+  $result = [];
+
+  foreach($json['questions'] as $questions) {
+    $arr['question'] = $questions['question'];
+
+    foreach($_POST as $key => $value ) {
+      if ($key === $questions['id']) {
+        $arr['userAnswer'] = $value;
+      }
+    }
+    foreach($questions['answers'] as $answers) {
+      if ($answers['isTrue'] === true) {
+        $arr['trueAnswer'] = $answers['answer'];
+      }
+    }
+    $result[] = $arr;
+  }
+  if ((count($_POST)-1) === count($json['questions'])) {
+    $allAnswers = true;
+  } else {
+    $message = 'Ответьте на все вопросы';
+  }
+  // print_r($result);
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -20,30 +64,44 @@ if (!empty($POST)) {
   <title>Test</title>
 </head>
 <body>
+<a href="admin.php">Загрузка тестов</a>
+<a href="list.php">Список тестов</a>
+<h3>Тема теста: "<?php echo $json['title']?>"</h3>
 <form action="test.php" method="POST">
+<input type="hidden" name="fileName" value="<?php echo $json['fileName']?>"/>
 <?php
-foreach ($jsonData as $question) { ?>
-  <legend><?php echo $question['question']?></legend>
-  <?php foreach ($question['answers'] as $answer) { ?>
-    <label>
-      <input type="radio" name="<?php echo $question['id']?>" value="<?php echo $answer['answer']?>"> 
-        <?php echo $answer['answer']?>
-    </label>
-  <?php } ?>
-<?php }
-?>
-  <div><input type="submit" value="Отправить"></div>
-</form>
-<h2>Результат</h2>
-<?php
-if (!empty($POST)) {
-  echo $POST;
-  var_dump($POST);
-}
-?>
-<!-- вопрос
-ответ
-результат -->
+if (empty($_POST)) {
+  foreach ($json['questions'] as $question) { ?>
+    <legend><?php echo $question['question']?></legend>
+    <?php foreach ($question['answers'] as $answer) { ?>
+      <label>
+        <input type="radio" name="<?php echo $question['id']?>" value="<?php echo $answer['answer']?>"> 
+          <?php echo $answer['answer']?>
+      </label>
+    <?php } ?>
   
+  <?php }
+  ?>
+  <div>
+  <input type="submit" value="Отправить">
+
+</div>
+  
+<?php } ?>
+</form>
+
+<?php
+if (!empty($_POST) && isset($allAnswers)) { ?>
+<h3>Результаты теста:</h3>
+<?php foreach($result as $item) { ?>
+  <h4><?php echo $item['question']?></h4>
+  <p>Ваш ответ: <?php echo $item['userAnswer']?></p>
+  <p>Правильный ответ: <?php echo $item['trueAnswer']?></p>
+  <?php } ?>
+<?php } else { ?>
+  <p><?php echo $message?></p>
+  <?php }
+?>
+
 </body>
 </html>
