9afe2910e7e1cc26ee94e4588dd2526f44a41649
NataliaFomina <38000375+NataliaFomina@users.noreply.github.com>
2019-01-07 19:20:55 +0300

    Create index.php

---
+++ 2.4/testSession/index.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+$message = " ";
+
+if (isset($_POST['username'], $_POST['password'])) {
+  // print_r($_POST);
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if (file_exists('login.json')) {
+    $file = file_get_contents('login.json', true);
+    $users = json_decode($file, true);
+    foreach ($users as $user) {
+      if ($user['name'] === $username  && $user['password'] === $password) {
+        $_SESSION['admin'] = $username;
+        header('Location: list.php');
+      } else {
+        $message = "Неверное имя пользователя или пароль";
+      }
+    }
+  }
+}
+
+if (isset($_POST['guestname'])) {
+  $_SESSION['guest'] = $_POST['guestname'];
+  header('Location: list.php');
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Index</title>
+</head>
+<body>
+<h3>Форма авторизации</h3>
+<form action="index.php" method="POST">
+  <legend>Введите имя пользователя</legend>
+  <input type="text" name="username"/>
+  <br/>
+  <legend>Введите пароль</legend>
+  <input type="text" name="password"/>
+  <input type="submit">
+</form>
+<p style="color:red;"><?php echo $message ?></p>
+
+<h3>Или войдите как гость</h3>
+<form action="index.php" method="POST">
+  <legend>Введите имя пользователя</legend>
+  <input type="text" name="guestname"/>
+  <br/>
+  <input type="submit">
+</form>
+ 
+</body>
+</html>
